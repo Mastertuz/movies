ecd92ea22aec50e495684d7c69789c9562d08a9b
Mastertuz <futureprogrammer2301@gmail.com>
2025-03-25 21:31:35 +0300

    fixing hover on mobile devices

--- src/components/shared/Pagination.tsx
+++ src/components/shared/Pagination.tsx
@@ -8,10 +8,12 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { usePathname, useSearchParams } from "next/navigation";
+
 function PaginationComponent() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get("page")) || 1;
+
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set("page", pageNumber.toString());
@@ -23,42 +25,57 @@ function PaginationComponent() {
       <PaginationContent>
         <PaginationItem>
           <PaginationPrevious
-            href={createPageURL(
-              currentPage > 1 ? currentPage - 1 : currentPage
-            )}
+            href={createPageURL(currentPage > 1 ? currentPage - 1 : currentPage)}
+            className="hover:bg-gray-200 active:bg-transparent"
           />
         </PaginationItem>
 
         <PaginationItem>
           {currentPage > 1 && (
-            <PaginationLink href={createPageURL(currentPage - 1)}>
+            <PaginationLink
+              href={createPageURL(currentPage - 1)}
+              className="hover:bg-gray-200 active:bg-transparent"
+            >
               {currentPage - 1}
             </PaginationLink>
           )}
         </PaginationItem>
 
         <PaginationItem>
-          <PaginationLink href={createPageURL(currentPage)} isActive>
+          <PaginationLink
+            href={createPageURL(currentPage)}
+            isActive
+            className="hover:bg-gray-200 active:bg-transparent"
+          >
             {currentPage}
           </PaginationLink>
         </PaginationItem>
 
         <PaginationItem>
-          <PaginationLink href={createPageURL(currentPage + 1)}>
+          <PaginationLink
+            href={createPageURL(currentPage + 1)}
+            className="hover:bg-gray-200 active:bg-transparent"
+          >
             {currentPage + 1}
           </PaginationLink>
         </PaginationItem>
 
-        {currentPage == 1 && (
+        {currentPage === 1 && (
           <PaginationItem>
-            <PaginationLink href={createPageURL(currentPage + 2)}>
+            <PaginationLink
+              href={createPageURL(currentPage + 2)}
+              className="hover:bg-gray-200 active:bg-transparent"
+            >
               {currentPage + 2}
             </PaginationLink>
           </PaginationItem>
         )}
 
         <PaginationItem>
-          <PaginationNext href={createPageURL(currentPage + 1)} />
+          <PaginationNext
+            href={createPageURL(currentPage + 1)}
+            className="hover:bg-gray-200 active:bg-transparent"
+          />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
